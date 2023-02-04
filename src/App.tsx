f033e64b9473071c1054d1ccbbec8bcb414785c1
lizuncong <2900170896@qq.com>
2023-02-04 15:50:03 +0800

    feat: stash

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
-import React, { memo } from "react";
-import bridge from "./bridge";
-import comp from "./bridge/components";
-import OpenWindow from "./open-window";
-// @ts-ignore
-const Table = React.lazy(() => comp("Table"));
+// @ts-nocheck
+import { memo, useState } from "react";
 
-function MyApp() {
-  console.log("MyApp...");
-  return <OpenWindow />;
-}
+import Modal from "./components/modal";
+import './util'
+import * as test from './test'
+console.log('test...', test)
+test.c()
+const App = memo(() => {
+  const [visible, setVisible] = useState(false);
+  const [visible2, setVisible2] = useState(false);
+  return (
+    <>
+      <div>
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? "关闭" : "打开"}
+        </button>
+      </div>
+      <div>
+        <button onClick={() => setVisible2(!visible2)}>
+          {visible2 ? "关闭下发课件弹窗" : "打开下发课件弹窗"}
+        </button>
+      </div>
+      <Modal
+        visible={visible}
+        closeAfterBlur={false}
+        name="app-modal"
+        winOptions={{ width: 100, height: 100, x: 0, y: 200 }}
+        onClose={() => {
+          setVisible(false);
+        }}
+      >
+        <div>app</div>
+      </Modal>
+      <Modal
+        visible={visible2}
+        closeAfterBlur={false}
+        name="courseware-modal"
+        winOptions={{ width: 100, didNotHideEntry: true, height: 100, x: 0, y: 300 }}
+        onClose={() => {
+          setVisible2(false);
+        }}
+      >
+        <div>courseware</div>
+        <button onClick={() => setVisible2(false)}>close</button>
+      </Modal>
+    </>
+  );
+});
 
-export default memo(MyApp);
+export default App;
---
+++ src/util.js
@@ -0,0 +1,5 @@
+import * as util from './test'
+
+util.c = () => {
+    console.log('c=====')
+}
