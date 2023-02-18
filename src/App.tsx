c1eb69c0f79fb62b998f357d0084a64803a67aeb
lizuncong <2900170896@qq.com>
2023-02-18 22:26:28 +0800

    feat: 性能

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 // @ts-nocheck
 import { memo, useState } from "react";
 
+import img1 from "./imgs/1.jpg";
+import img2 from "./imgs/2.jpg";
+import img3 from "./imgs/2.png";
+import img4 from "./imgs/3.png";
+import img5 from "./imgs/4.jpg";
+import img6 from "./imgs/5.gif";
 import Modal from "./components/modal";
-import './util'
-import * as test from './test'
-console.log('test...', test)
-test.c()
+
 const App = memo(() => {
   const [visible, setVisible] = useState(false);
   const [visible2, setVisible2] = useState(false);
   return (
-    <>
+    <div>
+      <div id="memory"></div>
       <div>
-        <button onClick={() => setVisible(!visible)}>
-          {visible ? "关闭" : "打开"}
-        </button>
+        <button id="clear">清空缓存</button>
       </div>
-      <div>
-        <button onClick={() => setVisible2(!visible2)}>
-          {visible2 ? "关闭下发课件弹窗" : "打开下发课件弹窗"}
-        </button>
-      </div>
+      首页{" "}
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        toggle
+      </button>
+      <button
+        onClick={() => {
+          setVisible2(!visible2);
+        }}
+      >
+        toggle2
+      </button>
       <Modal
         visible={visible}
         closeAfterBlur={false}
@@ -30,21 +42,36 @@ const App = memo(() => {
           setVisible(false);
         }}
       >
-        <div>app</div>
+        <div>
+          {[img1, img2, img3, img4, img5, img6].map((src) => {
+            return (
+              <div key={src}>
+                <img src={src} alt="" />
+              </div>
+            );
+          })}
+        </div>
       </Modal>
       <Modal
         visible={visible2}
         closeAfterBlur={false}
-        name="courseware-modal"
-        winOptions={{ width: 100, didNotHideEntry: true, height: 100, x: 0, y: 300 }}
+        name="app-modal2"
+        winOptions={{ width: 100, height: 100, x: 200, y: 200 }}
         onClose={() => {
           setVisible2(false);
         }}
       >
-        <div>courseware</div>
-        <button onClick={() => setVisible2(false)}>close</button>
+        <div>
+          {[img1, img2, img3, img4, img5, img6].map((src) => {
+            return (
+              <div key={src}>
+                <img src={src} alt="" />
+              </div>
+            );
+          })}
+        </div>
       </Modal>
-    </>
+    </div>
   );
 });
 
